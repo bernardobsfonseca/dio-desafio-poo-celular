6044112db872768b5e38423af827ce2b657f0155
bernardobsfonseca <bernardo.bs.fonseca@proton.me>
2024-01-04 15:02:30 -0300

    commit do projeto final

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main()
+        {
+            Nokia nokia = new Nokia("14998334276", "3310", "857438824351908", 16);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("Snake");
+
+            Console.WriteLine();
+
+            Iphone iphone = new Iphone("14983987865", "X", "844573421098701", 64);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+
+            Console.ReadLine();
+        }
+    }
+}
